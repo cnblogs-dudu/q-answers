7e5f8ded6736235ea4c2d68337f0f78eb7223d20
dudu <dudu@cnblogs.com>
2018-01-10 21:56:11 +0800

    Answer to https://q.cnblogs.com/q/102939/

---
+++ Q102939/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Q102939
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                using (var ms = new MemoryStream())
+                {
+                    var orgBodyStream = context.Response.Body;
+                    context.Response.Body = ms;
+
+                    await next();
+
+                    using (var sr = new StreamReader(ms))
+                    {
+                        ms.Seek(0, SeekOrigin.Begin);
+                        Console.WriteLine("Read Response.Body:\n" + sr.ReadToEnd());
+
+                        ms.Seek(0, SeekOrigin.Begin);
+                        await ms.CopyToAsync(orgBodyStream);
+                        context.Response.Body = orgBodyStream;
+                    }
+                }
+            });
+
+            app.UseMvc();
+        }
+    }
+}
