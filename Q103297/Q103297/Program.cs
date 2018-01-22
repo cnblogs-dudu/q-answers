fc7deb54abc684b82e83c2b4c7f5f2bcdb6233e9
dudu <dudu@cnblogs.com>
2018-01-22 22:40:10 +0800

    For https://q.cnblogs.com/q/103297/

---
+++ Q103297/Q103297/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q103297
+{
+    class Program
+    {
+        private static StreamWriter _writer = new StreamWriter(@"C:\temp\test.txt", true, Encoding.UTF8) { AutoFlush = true };
+
+        static async Task Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                await _writer.WriteLineAsync(i.ToString());
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds + "ms");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                _writer.WriteLine(i);
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds + "ms");
+        }
+    }
+}
