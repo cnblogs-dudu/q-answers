eaab03b02bc935517e1ef1354b616111bf9d50b7
dudu <dudu@cnblogs.com>
2018-02-13 20:58:57 +0800

    For https://q.cnblogs.com/q/97265/

---
+++ Q92475/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Q92475
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(outputTemplate: 
+                "{Timestamp:HH:mm:ss} [{Level}] {SourceContext} {NewLine}{Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            var webHost = WebHost.CreateDefaultBuilder(args)
+                .Configure(_ => { })
+                .UseSerilog()                
+                .Build();
+
+            var logger = webHost.Services.GetService<ILoggerFactory>().CreateLogger<Program>();
+            logger.LogError("an error occured");
+        }
+    }
+}
